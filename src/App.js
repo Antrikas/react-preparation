68b9ec841bdf07a6c846041ad870376b83cb5052
Antrikas <andreas.antoniades17@gmail.com>
2024-02-05 11:37:04 +0200

    react-preparation

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import  { BrowserRouter as Router, Routes, Route }  from 'react-router-dom'
+import Home from './pages/Home'
+import Posts from './pages/Posts'
 
 function App() {
   return (
     <div className="App">
-      
+      <Router>
+        <div className="App">
+          <Routes>
+            <Route path='/' element={<Home />}></Route>
+            <Route path=':id'element={<Posts />}></Route>
+          </Routes>
+        </div>
+      </Router>
     </div>
   );
 }
